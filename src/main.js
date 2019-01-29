37ed1ca9834c77a4dd36da99b9fb775806508a22
lazyTimes <1097483508@qq.com>
2019-01-29 16:37:58 +0800

    这是webstorm测试提交

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@
 // console.log('OK')
 
 import Vue from 'vue'
+//路由模块
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
 
 //导入自定义组件
 import app from './App.vue'
@@ -12,8 +15,15 @@ Vue.component(Header.name, Header);
 
 //导入mui样式
 import './lib/mui/css/mui.css'
+import './lib/mui/css/icons-extra.css'
+
+//导入自己的路由模块
+import router from './router'
 
 var vm = new Vue({
     el: '#app',
-    render: c => c(app)
+    render: c => c(app),
+    //挂载到实例
+    router
+
 });
